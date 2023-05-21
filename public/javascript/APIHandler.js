8ee3df2941acd20b46f11444676ccdd3e2243f16
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2023-05-22 01:10:50 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,75 @@
 class APIHandler {
   constructor (baseUrl) {
+    this.api = axios.create({baseUrl: baseUrl})
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+      return this.api.get("/characters")
+        .then(response => {
+          console.log("Retriving all the characters");
+          console.log("response data", response.data);
+          return response.data; 
+        })
+        .catch(()=> {
+          console.log("something went wrong");
+          return "ERROR"
+        })
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return this.api
+      .get(`/characters/${id}`)
+      .then(response => {
+        console.log("Retrieving a single character");
+        console.log("response data", response.data);
+        return response.data;
+      })
+      .catch(() => {
+        console.log("something went wrong");
+        return "ERROR";
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(data) {
+    return this.api
+      .post("/characters", data)
+      .then(response => {
+        console.log("Creating a character");
+        console.log("response data", response.data);
+        return response.data;
+      })
+      .catch(() => {
+        console.log("something went wrong");
+        return "ERROR";
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, data) {
+    return this.api
+      .put(`/characters/${id}`, data)
+      .then(response => {
+        console.log("Updating a character");
+        console.log("response data", response.data);
+        return response.data;
+      })
+      .catch(() => {
+        console.log("something went wrong");
+        return "ERROR";
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return this.api
+      .delete(`/characters/${id}`)
+      .then(response => {
+        console.log("Deleting a character");
+        console.log("response data", response.data);
+        return response.data;
+      })
+      .catch(() => {
+        console.log("something went wrong");
+        return "ERROR";
+      });
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,40 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:3000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList()
+    .then(infoCharacter => {
+      console.log(infoCharacter)
+    })
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    charactersAPI.getOneRegister()
+    .then(infoCharacter => {
+      console.log(infoCharacter)
+    })
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister()
+    .then(infoCharacter => {
+      console.log(infoCharacter)
+    })
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    charactersAPI.updateOneRegister()
+    .then(infoCharacter => {
+      console.log(infoCharacter)
+    })
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    charactersAPI.createOneRegister()
+    .then(infoCharacter => {
+      console.log(infoCharacter)
+    })
   });
 });
